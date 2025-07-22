e77c2526cc3e45d274e868860628887846e020cb
Joseph James <Joe.Centurion.5@gmail.com>
2025-07-22 18:35:26 +0100

    Skeleton Made

---
+++ file_duplicator/Driver.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Driver
+{
+    public String Folder_Name => $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\Downloads";
+    public static void Main(String[] args)
+    {
+        return;
+    }
+}
---
+++ file_duplicator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Security.Cryptography;
+public class Hasher
+{
+    public void Run(String Folder_Name)
+    {
+        if (Directory.Exists(Folder_Name))
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+    public void Hash()
+    {
+        return;
+    }
+}
