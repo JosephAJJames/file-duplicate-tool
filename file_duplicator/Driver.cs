04d25b44b1c91fe7a83fd9c823af864a5d1d8d4b
Joseph James <Joe.Centurion.5@gmail.com>
2025-07-22 19:01:28 +0100

    Loops Through Download Folder

--- file_duplicator/Driver.cs
+++ file_duplicator/Driver.cs
@@ -5,6 +5,8 @@ public class Driver
     public String Folder_Name => $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\Downloads";
     public static void Main(String[] args)
     {
-        return;
+        Driver driver = new Driver();
+        Hasher hasher = new Hasher();
+        hasher.Run(driver.Folder_Name);
     }
 }
--- file_duplicator/Program.cs
+++ file_duplicator/Program.cs
@@ -4,13 +4,18 @@ public class Hasher
 {
     public void Run(String Folder_Name)
     {
-        if (Directory.Exists(Folder_Name))
+        if (!Directory.Exists(Folder_Name))
         {
-
+            return;
         }
-        else
-        {
 
+        String[] files = Directory.GetFiles(Folder_Name);
+        foreach (String file in files)
+        {
+            using (FileStream fs = File.Open(file, FileMode.Open))
+            {
+                Console.WriteLine(fs);
+            }
         }
     }
     public void Hash()
