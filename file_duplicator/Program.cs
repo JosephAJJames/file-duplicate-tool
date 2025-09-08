3684e02a5e1d0f532ce45e592c89afdf8d328dcc
Joseph James <Joe.Centurion.5@gmail.com>
2025-09-08 20:24:52 +0100

    Works

--- file_duplicator/Program.cs
+++ file_duplicator/Program.cs
@@ -3,11 +3,15 @@ using System.Net;
 using System.Runtime.Intrinsics.Arm;
 using System.Security.Cryptography;
 using System.Collections;
+using System.Runtime.CompilerServices;
 public class Hasher
 {
-    private Dictionary<string, int> table = new Dictionary<string, int>();
+    private Dictionary<string, int> hash_to_frequeny = new Dictionary<string, int>();
+
+    private Dictionary<string, ArrayList> hash_to_name = new Dictionary<string, ArrayList>();
     public void Run(String Folder_Name)
     {
+
         if (!Directory.Exists(Folder_Name))
         {
             return;
@@ -20,23 +24,75 @@ public class Hasher
 
             if (Hash_Result != null)
             {
+                this.create_hash_to_file(Convert.ToBase64String(Hash_Result), file);
                 this.add(Convert.ToBase64String(Hash_Result));
             }
         }
 
-        Console.WriteLine(this.table);
+        List<string> file_sigs = this.get_duplicates();
+        foreach (string file_sig in file_sigs)
+        {
+            ArrayList files_to_be_deleted = this.hash_to_name[file_sig];
+            delete_files(files_to_be_deleted, Folder_Name);
+        }
+
+    }
+
+    public void create_hash_to_file(string hash, string name)
+    {
+        if (!hash_to_name.ContainsKey(hash))
+        {
+
+            ArrayList list = new ArrayList();
+            list.Add(name);
+            hash_to_name[hash] = list;
+        }
+        else
+        {
+
+            hash_to_name[hash].Add(name);
+        }
+    }
+
+    public void delete_files(ArrayList file_sigs, string Folder_Name)
+    {
+        string targetFolder = Path.Combine(Folder_Name, "Duplicates");
+        for (int x = 0; x < file_sigs.Count - 1; x++)
+        {
+            string org_path = (string)file_sigs[x];
+            string file_Name = Path.GetFileName(org_path);
+            string dest = Path.Combine(targetFolder, file_Name);
+
+            File.Move(org_path, dest);
+
+        }
     }
 
     public void add(string Hash_Result)
     {
-        if (!table.ContainsKey(Hash_Result))
+        if (!hash_to_frequeny.ContainsKey(Hash_Result))
         {
-            table[Hash_Result] = 1;
+            hash_to_frequeny[Hash_Result] = 1;
         }
         else
         {
-            table[Hash_Result] = (int)table[Hash_Result] + 1;
+            hash_to_frequeny[Hash_Result] = (int)hash_to_frequeny[Hash_Result] + 1;
         }
+    }
+
+    public List<string> get_duplicates()
+    {
+        List<string> file_sigs = new List<string>();
+        foreach (string key in this.hash_to_frequeny.Keys)
+        {
+            if (this.hash_to_frequeny[key] > 1)
+            {
+                file_sigs.Add(key);
+            }
+        }
+
+
+        return file_sigs;
     }
 
     public byte[]? Hash(String file)
